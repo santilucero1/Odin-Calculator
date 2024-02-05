71af8ee2b571546426fde749ba3b34580a99418e
Santiago Lucero <61132148+santilucero1@users.noreply.github.com>
2024-02-05 18:13:10 -0300

    Works!

--- javascript.js
+++ javascript.js
@@ -44,68 +44,90 @@ function operate (a,b,o) {
 let arrayN = []
 let inputA = true;
 let result;
+var pElement = document.querySelector('.pdisplay');
+
+
 
 document.addEventListener("click", (e) =>{
 
-    if(inputA){
-        
-        //if press any operator
-        if (['+', '-', '*', '/', '='].includes(e.target.innerText)){
-    
-            o = e.target.innerText; //get operation
-            inputA = false; //next input
-        }
-
-        else {
-        a = a+ e.target.innerText //concatenate 
-        console.log(a)}
-    
-    }
-
-    else{
-
-        //if press any operator
-        if (['+', '-', '*', '/', '='].includes(e.target.innerText)){
+    if (e.target.tagName === 'BUTTON') {
+        if(inputA){
             
-            //convert a and b to int
-            a = parseInt(a)
-            b = parseInt(b)
+            //if press any operator
+            if (['+', '-', '*', '/', '='].includes(e.target.innerText)){
+        
+                o = e.target.innerText; //get operation
+                pElement.textContent = a+o
+                inputA = false; //next input
+            }
 
-            //make the operations
-            switch (o) {
+            else if (e.target.innerText == "C"){
+                a= a.slice(0, -1);
+                pElement.textContent = a;
+            }
 
-                case "+":
-                  result = add(a, b);
-                  console.log(result, "add");
-                  break;
-
-                case "-":
-                  result = subtract(a, b);
-                  console.log(result, "substract");
-                  break;
-
-                case "/":
-                  result = divide(a, b);
-                  console.log(result, "divide");
-                  break;
-
-                case "*":
-                  result = multiply(a, b);
-                  console.log(result, "multiply");
-                  break;
-                }
+            else {
                 
-            o = e.target.innerText; //get operation
-            a = result;
-            b = "";
+            a = a+ e.target.innerText //concatenate
+            pElement.textContent = a;
+            //console.log(a)
+            }
+            
+        
         }
 
-        else {
-            b = b+ e.target.innerText //concatenate 
-            console.log(b)}
+        else{
+
+            //if press any operator
+            if (['+', '-', '*', '/', '='].includes(e.target.innerText)){
+                
+                //convert a and b to int
+                a = parseInt(a)
+                b = parseInt(b)
+
+                //make the operations
+                switch (o) {
+
+                    case "+":
+                    result = add(a, b);
+                    console.log(result, "add");
+                    break;
+
+                    case "-":
+                    result = subtract(a, b);
+                    console.log(result, "substract");
+                    break;
+
+                    case "/":
+                    result = divide(a, b);
+                    console.log(result, "divide");
+                    break;
+
+                    case "*":
+                    result = multiply(a, b);
+                    console.log(result, "multiply");
+                    break;
+                    }
+                    
+            
+                o = e.target.innerText; //get operation
+                a = result;
+                b = "";
+            }
+
+            else if (e.target.innerText == "C") {
+
+                b= b.slice(0, -1);
+                pElement.textContent = b;
+            }
+            
+            else {
+                b = b+ e.target.innerText //concatenate 
+                console.log(b)}
+                pElement.textContent = (a+o+b).replace('=', '');
+            }
         }
-    }
-   
+}
 )
 //the numbers pressed will be saved in one array once at time
 //then need to convert to string with Join method
